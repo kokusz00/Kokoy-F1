12317f34cd593ebecebd4b65a09643c7881ea2d2
kokusz00 <dnrradar@gmail.com>
2025-06-05 18:36:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
+// main.js
+let isOn = false;
+const screen = document.getElementById('screen');
+const powerBtn = document.getElementById('powerBtn');
+
+powerBtn.addEventListener('click', () => {
+  isOn = !isOn;
+  if (isOn) {
+    loadScreen('startup');
+    setTimeout(() => loadScreen('home'), 4000);
+  } else {
+    screen.innerHTML = '';
+  }
+});
+
+function loadScreen(name) {
+  fetch(`screens/${name}.html`)
+    .then(res => res.text())
+    .then(html => {
+      screen.innerHTML = html;
+      const cssLink = document.createElement('link');
+      cssLink.rel = 'stylesheet';
+      cssLink.href = `screens-css/${name}.css`;
+      document.head.appendChild(cssLink);
+
+      const script = document.createElement('script');
+      script.src = `screens-js/${name}.js`;
+      document.body.appendChild(script);
+    });
+}
 
