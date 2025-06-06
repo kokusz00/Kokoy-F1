fb04d7222e81412a5b9ac603ee00a7a7db667e1b
kokusz00 <dnrradar@gmail.com>
2025-06-06 16:38:16 +0200

    Update startup.js

--- startup.js
+++ startup.js
@@ -1,18 +1,13 @@
-window.onload = () => {
+window.addEventListener('DOMContentLoaded', () => {
   const logo = document.getElementById("logo");
   const powered = document.getElementById("powered");
 
   setTimeout(() => {
-    logo.classList.remove("hidden");
-    logo.classList.add("show");
+    logo.style.opacity = 1;
+    powered.style.opacity = 1;
   }, 1000);
-
-  setTimeout(() => {
-    powered.classList.remove("hidden");
-    powered.classList.add("show");
-  }, 3000);
 
   setTimeout(() => {
     window.location.href = "unlock.html";
   }, 6000);
-};
+});
